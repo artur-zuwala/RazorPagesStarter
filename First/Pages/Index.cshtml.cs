2442e391eb861fa9dea87904d4b833bfc42e6c1f
Artur Żuwała <100224740+artur-zuwala@users.noreply.github.com>
2024-12-21 19:14:17 +0100

    Add project files.

---
+++ First/Pages/Index.cshtml.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace First.Pages;
+public class IndexModel : PageModel
+{
+    public int Counter { get; private set; }
+    public void OnGet()
+    {
+        Counter = HttpContext.Session.GetInt32("Counter") ?? 1;
+    }
+    public void OnPost()
+    {
+        Counter = HttpContext.Session.GetInt32("Counter") ?? 1;
+        Counter++;
+        HttpContext.Session.SetInt32("Counter", Counter);
+    }
+}
